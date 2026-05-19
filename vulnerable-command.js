ac7e0b80dcaf079b1f30dfb370dfb1073915de1e
elipielet <eli.pielet@checkmarx.com>
2026-05-19 12:56:05 +0300

    Checkmarx Bot - Auto Pull Request

--- vulnerable-command.js
+++ vulnerable-command.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const { exec } = require("child_process");
+const { execFile } = require("child_process");
 
 const app = express();
 
 app.get("/ping", (req, res) => {
   const host = req.query.host;
 
-  exec("ping -c 1 " + host, (error, stdout, stderr) => {
+  // Use execFile with arguments array to prevent command injection
+  execFile("ping", ["-c", "1", host], (error, stdout, stderr) => {
     if (error) {
       return res.status(500).send(error.message);
     }
